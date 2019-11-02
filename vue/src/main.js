a22323c00e77fb02b16406963d280e95f9f57e86
Andrew Kim <akinwahn@gmail.com>
2019-11-02 11:39:59 -0400

    Eslint. FormWizard, TabContent.

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCamera from '@mrjeffapp/vuejs-camera'
-
+import Chat from 'vue-beautiful-chat'
 
 // import 'vue2-dropzone/dist/vue2Dropzone.css'
 
@@ -14,3 +14,4 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(VueCamera);
+Vue.use(Chat)
--- vue/src/views/Upload.vue
+++ vue/src/views/Upload.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="upload">
       <h1>Upload</h1>
-      <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+      <form-wizard>
+        <tab-content title="Personal details">
+            My first tab content
+        </tab-content>
+        <tab-content title="Additional Info">
+            My second tab content
+        </tab-content>
+        <tab-content title="Last step">
+            Yuhuuu! This seems pretty damn simple
+        </tab-content>
+        <tab-content title="Last step">
+            <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
       <button @click="removeAllFiles">Remove All Files</button>
+    </tab-content>
+        <tab-content title="Last step">
+            <VueCamera />
+        </tab-content>
+        </form-wizard>
+      
 
       <br>
       <br>
-      <VueCamera />
+      
 
   </div>
 </template>
@@ -15,13 +32,17 @@
 import vue2Dropzone from 'vue2-dropzone'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 import VueCamera from '@mrjeffapp/vuejs-camera'
+import {FormWizard, TabContent} from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
 
 
 export default {
     name: 'upload',
   components: {
     vueDropzone: vue2Dropzone,
-    VueCamera: VueCamera
+    VueCamera: VueCamera,
+    FormWizard,
+    TabContent
   },
   data: function () {
     return {
